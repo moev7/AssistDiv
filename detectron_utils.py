966360bea45b7480a91983eb59115124f4d36312
thehusnainahmad <husnaincheema8080@gmail.com>
2023-12-05 13:33:24 +0100

    categories

--- detectron_utils.py
+++ detectron_utils.py
@@ -3,11 +3,25 @@ from detectron2.data import MetadataCatalog
 from detectron2.engine import DefaultPredictor
 from detectron2.utils.visualizer import Visualizer, ColorMode
 from detectron2 import model_zoo
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data.catalog import MetadataCatalog
 import cv2
 import numpy as np
-from speech_utils import speak
+from speech_utils import speak, announce_objects, get_voice_input
+from translations import LANGUAGE
+
+
+def get_objects_by_position_categorized(detected_objects, language='en'):
+    categorized_objects = {category: [] for category in LANGUAGE[language].keys()}
+
+    for obj in detected_objects:
+        for category, objects in LANGUAGE[language].items():
+            if obj['name'] in objects:
+                categorized_objects[category].append(obj)
+
+    for category, objects in categorized_objects.items():
+        if objects:
+            speak(f"Category: {category}", language)
+            for i, obj in enumerate(objects, start=1):
+                speak(f"{i} - {LANGUAGE[language].get(obj['name'], obj['name'])} at {obj['distance']} meters", language)
 
 
 def get_objects_by_position(detected_objects, language='en'):
@@ -15,19 +29,17 @@ def get_objects_by_position(detected_objects, language='en'):
     front_objects = []
     right_objects = []
 
-    image_width = 640 
-    
+    image_width = 640
+
     left_threshold = image_width // 3
     right_threshold = (2 * image_width) // 3
-    
+
     for obj in detected_objects:
-        # Get all the x coordinates of the pixels occupied by the object
         x_pixels = np.where(obj['mask'])[1]
         left_pixels = np.sum(x_pixels < left_threshold)
         right_pixels = np.sum(x_pixels > right_threshold)
         front_pixels = np.sum((x_pixels >= left_threshold) & (x_pixels <= right_threshold))
 
-        # Choose the section where the object has the most pixels
         if left_pixels > right_pixels and left_pixels > front_pixels:
             left_objects.append(obj)
         elif right_pixels > left_pixels and right_pixels > front_pixels:
@@ -35,38 +47,27 @@ def get_objects_by_position(detected_objects, language='en'):
         else:
             front_objects.append(obj)
 
-    # Round the distance to one decimal place for each object
     for obj in left_objects:
         obj["distance"] = round(obj["distance"], 1)
     for obj in front_objects:
         obj["distance"] = round(obj["distance"], 1)
     for obj in right_objects:
         obj["distance"] = round(obj["distance"], 1)
-    
-    def speak_with_language(text, language='en'):
-        if language == 'en':
-            speak(text, language)
-        elif language == 'es':
-            translated_text = spanish_translations.get(text, text)
-            speak(translated_text, language)
 
-    speak_with_language("There are " + str(len(detected_objects)) + " objects detected in the scene.", language)
-        
     if left_objects:
-        speak_with_language("Objects on the left side are:", language)
+        speak("Objects on the left side are:", language)
         for obj in left_objects:
-            speak_with_language(f"{obj['name']} at {obj['distance']} meters", language)
+            speak(f"{LANGUAGE[language].get(obj['name'], obj['name'])} at {obj['distance']} meters", language)
 
     if front_objects:
-        speak_with_language("\nObjects in front of you are:", language)
+        speak("\nObjects in front of you are:", language)
         for obj in front_objects:
-            speak_with_language(f"{obj['name']} at {obj['distance']} meters", language)
+            speak(f"{LANGUAGE[language].get(obj['name'], obj['name'])} at {obj['distance']} meters", language)
 
     if right_objects:
-        speak_with_language("\nObjects on your right side are:", language)
+        speak("\nObjects on your right side are:", language)
         for obj in right_objects:
-            speak_with_language(f"{obj['name']} at {obj['distance']} meters", language)
-
+            speak(f"{LANGUAGE[language].get(obj['name'], obj['name'])} at {obj['distance']} meters", language)
 
 
 def run_object_detection(predictor, color_image):
@@ -74,109 +75,17 @@ def run_object_detection(predictor, color_image):
     return outputs
 
 
-    # Spanish translations for class names
-spanish_translations = {
-                "person": "persona",
-                "bicycle": "bicicleta",
-                "car": "coche",
-                "motorcycle": "motocicleta",
-                "airplane": "avión",
-                "bus": "autobús",
-                "train": "tren",
-                "truck": "camión",
-                "boat": "barco",
-                "traffic light": "semáforo",
-                "fire hydrant": "hidrante",
-                "stop sign": "señal de stop",
-                "parking meter": "parquímetro",
-                "bench": "banco",
-                "bird": "pájaro",
-                "cat": "gato",
-                "dog": "perro",
-                "horse": "caballo",
-                "sheep": "oveja",
-                "cow": "vaca",
-                "elephant": "elefante",
-                "bear": "oso",
-                "zebra": "cebra",
-                "giraffe": "jirafa",
-                "backpack": "mochila",
-                "umbrella": "paraguas",
-                "handbag": "bolso",
-                "tie": "corbata",
-                "suitcase": "maleta",
-                "frisbee": "frisbee",
-                "skis": "esquís",
-                "snowboard": "tabla de snowboard",
-                "sports ball": "pelota de deporte",
-                "kite": "cometa",
-                "baseball bat": "bate de béisbol",
-                "baseball glove": "guante de béisbol",
-                "skateboard": "monopatín",
-                "surfboard": "tabla de surf",
-                "tennis racket": "raqueta de tenis",
-                "bottle": "botella",
-                "wine glass": "copa de vino",
-                "cup": "taza",
-                "fork": "tenedor",
-                "knife": "cuchillo",
-                "spoon": "cuchara",
-                "bowl": "bol",
-                "banana": "plátano",
-                "apple": "manzana",
-                "sandwich": "sándwich",
-                "orange": "naranja",
-                "broccoli": "brócoli",
-                "carrot": "zanahoria",
-                "hot dog": "perro caliente",
-                "pizza": "pizza",
-                "donut": "donut",
-                "cake": "pastel",
-                "chair": "silla",
-                "couch": "sofá",
-                "potted plant": "planta en maceta",
-                "bed": "cama",
-                "dining table": "mesa de comedor",
-                "toilet": "inodoro",
-                "tv": "televisión",
-                "laptop": "portátil",
-                "mouse": "ratón",
-                "remote": "mando a distancia",
-                "keyboard": "teclado",
-                "cell phone": "teléfono móvil",
-                "microwave": "microondas",
-                "oven": "horno",
-                "toaster": "tostadora",
-                "sink": "fregadero",
-                "refrigerator": "refrigerador",
-                "book": "libro",
-                "clock": "reloj",
-                "vase": "jarrón",
-                "scissors": "tijeras",
-                "teddy bear": "oso de peluche",
-                "hair drier": "secador de pelo",
-                "toothbrush": "cepillo de dientes",
-            }
-
-
-def visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg):
+def visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg, language='en'):
     outputs = run_object_detection(predictor, color_image)
     v = Visualizer(color_image[:, :, ::-1], metadata=MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), instance_mode=ColorMode.IMAGE)
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
     detected_objects = []
 
-# ...
+    distance_image = np.zeros((color_image.shape[0], 200, 3), dtype=np.uint8)
+    text_position_start = 30
 
-# Choose the appropriate language for the text based on the selected language
-    language = 'es'  # Change the language to 'es' for Spanish
-
-# ...
-
-    # Create a black image to display distances
-    distance_image = np.zeros((color_image.shape[0], 200, 3), dtype=np.uint8)  # width of 200px
-
-    text_position_start = 30  # y position to start writing text
+    categorized_objects = {category: [] for category in LANGUAGE[language].keys()}
 
     for i, (class_idx, mask, box) in enumerate(zip(outputs["instances"].pred_classes.to("cpu"), outputs["instances"].pred_masks.to("cpu"), outputs["instances"].pred_boxes.tensor)):
         instance_mask = mask.cpu().numpy().astype(np.uint8)
@@ -184,72 +93,83 @@ def visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
         distances = masked_depth_image[np.nonzero(masked_depth_image)]
 
         if len(distances) > 0:
-            mean_distance = np.mean(distances) / 1000  # assuming the depth is in millimeters
+            mean_distance = np.mean(distances) / 1000
         else:
             mean_distance = 0
 
-        distance_text = f"{mean_distance:.2f} m"
         class_name = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes[class_idx]
+        translated_class_name = LANGUAGE[language].get(class_name, class_name)
 
         y, x = np.nonzero(instance_mask)
         x_center, y_center = int(np.mean(x)), int(np.mean(y))
 
         centroid = (x_center, y_center)
 
-        detected_objects.append({
+        detected_object = {
             "id": i,
-            "name": class_name,
+            "name": translated_class_name,
             "distance": mean_distance,
             "centroid": centroid,
             "box": box,
             "mask": instance_mask
-        })
+        }
 
-        # Inside the loop, set the text for each object based on the selected language
-        if language == 'en':
-            text = f"{class_name}: {distance_text}"
-        elif language == 'es':
-            translated_class_name = spanish_translations.get(class_name, class_name)
-            print(f"Original class name: {class_name}, Translated class name: {translated_class_name}")
-            text = f"{translated_class_name}: {distance_text}"
+        # Add the object to the categorized_objects
+        for category, objects in LANGUAGE[language].items():
+            if detected_object['name'] in objects:
+                categorized_objects[category].append(detected_object)
+                break
 
-        cv2.putText(distance_image, text, (10, text_position_start), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-        text_position_start += 30  # move down by 30px for the next text
+    for category, objects in categorized_objects.items():
+        if objects:
+            speak(f"Category: {category}", language)
+            for i, obj in enumerate(objects, start=1):
+                # If there is more than one object in the category, add numeration
+                numeration = f"-{i}" if len(objects) > 1 else ""
+                speak(f"{i} - {category}{numeration}: {LANGUAGE[language].get(obj['name'], obj['name'])} at {obj['distance']:.1f} meters", language)
 
-    # Concatenate the images horizontally after the loop
+    for category, objects in categorized_objects.items():
+        for i, obj in enumerate(objects, start=1):
+            # If there is more than one object in the category, add numeration
+            numeration = f"-{i}" if len(objects) > 1 else ""
+            text = f"{category}{numeration}: {LANGUAGE[language].get(obj['name'], obj['name'])} at {obj['distance']:.1f} m"
+            cv2.putText(distance_image, text, (10, text_position_start), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            text_position_start += 30
+
     output_image = out.get_image()[:, :, ::-1]
     images_concat = np.hstack((distance_image, output_image))
 
-    # ...
-
     cv2.imshow("Distances and Instance Segmentation", images_concat)
-    cv2.waitKey(1)  # Change this value as needed or use cv2.waitKey(0) to wait until any key is pressed
-    #cv2.destroyAllWindows()
+    cv2.waitKey(1)
 
-    # ...
-
-    cv2.waitKey(1)  # Change this value as needed or use cv2.waitKey(0) to wait until any key is pressed
-    #cv2.destroyAllWindows()
-
-    # Sort detected_objects based on the x-coordinate of centroids
     detected_objects.sort(key=lambda obj: obj['centroid'][0])
 
     return detected_objects
 
 
+
+
 def initialize_detectron():
     cfg = get_cfg()
-    #load model config and pretrained model
-    #cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
-    #cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
-    
-    #cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml"))
-    #cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml")
-
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml"))
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml")
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-
-    cfg.MODEL.DEVICE = "cuda" #cpu or cuda
+    cfg.MODEL.DEVICE = "cuda"
     predictor = DefaultPredictor(cfg)
     return predictor, cfg
+
+
+# Main execution
+predictor, cfg = initialize_detectron()
+color_image = np.zeros((480, 640, 3), dtype=np.uint8)  # Replace with your actual color image
+depth_image = np.zeros((480, 640), dtype=np.uint16)  # Replace with your actual depth image
+
+# Example: English
+language = 'en'
+detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg, language)
+get_objects_by_position_categorized(detected_objects, language)
+
+# Example: Spanish
+language = 'es'
+detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg, language)
+get_objects_by_position_categorized(detected_objects, language)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from camera_utils import initialize_camera, get_camera_frames
-from detectron_utils import initialize_detectron, run_object_detection, visualize_and_get_detected_objects, get_objects_by_position
+from detectron_utils import initialize_detectron, run_object_detection, visualize_and_get_detected_objects, get_objects_by_position, get_objects_by_position_categorized
 from sound_utils import play_beep_sound, play_obstacle_beep_sound
 from speech_utils import speak, announce_objects, get_voice_input
 from relationship_utils import describe_relationship
@@ -16,7 +16,6 @@ depth_frame, color_frame, depth_image, color_image, gyro_frame, accel_frame = ge
 predictor, cfg = initialize_detectron()
 print("FRAME WIDTH: ")
 
-#detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
 frame_width = color_image.shape[1]
 print(frame_width)
 section_width = frame_width // 3
@@ -24,29 +23,27 @@ section_width = frame_width // 3
 left_boundary = section_width
 right_boundary = section_width * 2
 
-
-
 # Define language actions
 language_actions = {
     'en': {
-        'welcome_message': 'Hello, Welcome to Assist Div. Say Scan the scene for scene understanding and Find objects for object detection. Say quit to exit assist div',
-        'scan_scene_message': 'Scanning the scene......',
+        'welcome_message': 'Hello, Welcome to Assist Div. Say "Scan the scene" for scene understanding and "Find objects" for object detection. Say "quit" to exit Assist Div.',
+        'scan_scene_message': 'Scanning the scene...',
         'find_objects_message': 'Finding objects. Please wait.',
         'beeping_message': 'The selected object is in range. Do you want to start beeping?',
         'left_side_message': 'The selected object is on the left side.',
         'right_side_message': 'The selected object is on the right side.',
         'not_in_frame_message': 'The selected object is not in the frame.',
-        'repeat_message': 'Say repeat to repeat the object names. Say return to do another task. say quit to exit assist div.'
+        'repeat_message': 'Say "repeat" to repeat the object names. Say "return" to do another task. Say "quit" to exit Assist Div.'
     },
     'es': {
-        'welcome_message': 'Hola, bienvenido a Assist Div. Diga Escanear la escena para comprenderla y Buscar objetos para detectar objetos. Diga "salir" para salir',
-        'scan_scene_message': 'Escaneando la escena......',
-        'find_objects_message': 'Buscando objetos. Por favor espera.',
+        'welcome_message': 'Hola, bienvenido a Assist Div. Diga "Escanear la escena" para comprenderla y "Buscar objetos" para detectar objetos. Diga "salir" para salir.',
+        'scan_scene_message': 'Escaneando la escena...',
+        'find_objects_message': 'Buscando objetos. Por favor, espera.',
         'beeping_message': 'El objeto seleccionado está dentro del rango. ¿Quieres comenzar a emitir pitidos?',
         'left_side_message': 'El objeto seleccionado está en el lado izquierdo.',
         'right_side_message': 'El objeto seleccionado está en el lado derecho.',
         'not_in_frame_message': 'El objeto seleccionado no está en el marco.',
-        'repeat_message': 'Di repetir para repetir los nombres de los objetos. Di regresar para hacer otra tarea. diga salir para salir de la división de asistencia.'
+        'repeat_message': 'Diga "repetir" para repetir los nombres de los objetos. Diga "regresar" para hacer otra tarea. Diga "salir" para salir de la división de asistencia.'
     }
 }
 
@@ -56,14 +53,13 @@ try:
 
         # Get camera frames
         depth_frame, color_frame, depth_image, color_image, gyro_frame, accel_frame = get_camera_frames(pipeline)
-        detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
-        
-        
-        if not beeping_enabled or not selected_obj_flag: 
-            detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
-            
+        #detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
+
+        if not beeping_enabled or not selected_obj_flag:
+            #detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
+
             # Language selection
-            speak("Speak English to select English or spanish para español")
+            speak("Speak English to select English or Spanish para español")
             user_input = 'spanish'#get_voice_input()
             print(user_input)
 
@@ -75,8 +71,7 @@ try:
                 speak(language_actions[language]['welcome_message'], language)
             elif user_input == 'quit' or user_input == 'salir':
                 exit()
-            
-            #user_input = input("Select 'u' for scene understanding and 'o' for object detection: ")
+
             user_input = 'escanear la escena'#get_voice_input()
             print(user_input)
 
@@ -90,29 +85,25 @@ try:
 
             repeat = True
             detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
-            get_objects_by_position(detected_objects)
+            get_objects_by_position(detected_objects, language)
 
-            while repeat == True:
-                    speak("Say repeat to repeat the object names. Say return to do another task. say quit to exit assist div.")
-                    user_input = get_voice_input()
-                    print(user_input)
-                    if user_input == 'repeat':
-                        get_objects_by_position(detected_objects)
-                    elif user_input == "return":
-                        repeat = False
-                    elif user_input == 'quit':
-                        break
-                        
+            while repeat:
+                speak("Say repeat to repeat the object names. Say return to do another task. Say quit to exit Assist Div.", language)
+                user_input = get_voice_input()
+                print(user_input)
+                if user_input == 'repeat':
+                    get_objects_by_position_categorized(detected_objects, language)
+                elif user_input == "return":
+                    repeat = False
+                elif user_input == 'quit':
+                    break
+
             if user_input == 'find objects':
                 detected_objects = visualize_and_get_detected_objects(predictor, color_image, depth_image, cfg)
-                
-                speak("Select one of the following objects to find where it's placed:")
-                '''
-                for i, obj in enumerate(detected_objects):
-                    speak(f"{i + 1}: {obj['name']}")
-                '''
-                
-                speak("Say Select to select an object and say Quit to exit AssistDiv.")
+
+                speak("Select one of the following objects to find where it's placed:", language)
+
+                speak("Say Select to select an object and say Quit to exit Assist Div.", language)
                 user_input = get_voice_input()
                 print(user_input)
 
@@ -124,11 +115,11 @@ try:
                 elif user_input == 'quit':
                     break
 
-        if selected_obj_flag and not beeping_enabled :
+        if selected_obj_flag and not beeping_enabled:
             selected_obj_name = selected_obj["name"]
             if any(obj["name"] == selected_obj_name for obj in detected_objects):
                 updated_distance = get_updated_distance(selected_obj, detected_objects)
-                speak("Say start beeping to enable beeping, or say quit to exit AssistDiv: ")
+                speak("Say start beeping to enable beeping, or say quit to exit Assist Div: ", language)
                 user_input = get_voice_input()
                 if user_input == 'start beeping':
                     beeping_enabled = True
@@ -144,23 +135,21 @@ try:
             if centroid != 0:
                 # Check if the object's centroid is in the center third of the frame
                 if centroid[0] < left_boundary:
-                    speak("Object is on your left side")
+                    speak("Object is on your left side", language)
                 elif centroid[0] > right_boundary:
-                    speak("Object is on your right side")
+                    speak("Object is on your right side", language)
                 else:
                     play_beep_sound(updated_distance)
             else:
-                speak("Objects is not in the frame")
-        #play_obstacle_beep_sound(depth_image)
-        
+                speak("Objects is not in the frame", language)
+
         if user_input == 'quit':
             break
-        
+
         # Exit the loop when 'q' is pressed
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(0)
         if key == ord("q"):
             break
 
 finally:
-    pipeline.stop()
-    cv2.destroyAllWindows()
+    pipeline.stop
